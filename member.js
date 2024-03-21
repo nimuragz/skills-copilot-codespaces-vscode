dccac1d397d07a544258e55d584d45677044a64c
nimuragz <147482819+nimuragz@users.noreply.github.com>
2024-03-21 08:16:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 26,
+        skills: ['HTML', 'CSS', 'JS'],
+        details: {
+            address: '123 Main St',
+            city: 'Miami',
+            state: 'FL'
+        }
+    };}
